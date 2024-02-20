c9c96d88c82fd5dcf025d36560a8c1980ccfa82e
seungjae94 이승재 <mathlife12345@gmail.com>
2024-02-20 21:58:17 +0900

    윈도우 종료 버튼 클릭시 업데이트 콜백을 호출하지 않고 바로 메시지 루프를 빠져나오도록 수정

--- PokemonFireRed/EnginePlatform/EngineWindow.cpp
+++ PokemonFireRed/EnginePlatform/EngineWindow.cpp
@@ -126,6 +126,11 @@ unsigned __int64 UEngineWindow::WindowMessageLoop(void(*_Update)(), void(*_End)(
 			// EngineWindow::WndProc로 윈도우 메시지를 처리한다.
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+
+			if (false == WindowLive)
+			{
+				break;
+			}
 		}
 
 		if (nullptr != _Update)
