5380fc5e41d119f7d4a9ae2f84286989d9bc502d
seungjae94 이승재 <mathlife12345@gmail.com>
2024-02-20 10:02:23 +0900

    오박사 연구소 대화 이벤트 구현

--- PokemonFireRed/Pokemon/InteriorOaksLabLevel.cpp
+++ PokemonFireRed/Pokemon/InteriorOaksLabLevel.cpp
@@ -1,8 +1,9 @@
-#include "InteriorOaksLabLevel.h"
+ï»¿#include "InteriorOaksLabLevel.h"
 #include <EngineBase/EngineDirectory.h>
 #include <EngineBase/EngineFile.h>
 #include <EngineCore/EngineResourcesManager.h>
 #include "InteriorDoor.h"
+#include "DialogueActor.h"
 
 UInteriorOaksLabLevel::UInteriorOaksLabLevel()
 {
@@ -16,12 +17,12 @@ void UInteriorOaksLabLevel::BeginPlay()
 {
 	UMapLevel::BeginPlay();
 
-	// ÇÃ·¹ÀÌ¾î ½ÃÀÛ À§Ä¡ ¼³Á¤
+	// í”Œë ˆì´ì–´ ì‹œìž‘ ìœ„ì¹˜ ì„¤ì •
 	UEventManager::SetPoint(GetName(), Global::PlayerName, { 6, 6 });
 	UEventManager::SetDirection(GetName(), Global::PlayerName, FTileVector::Up);
 
 
-	// ÀÌº¥Æ® Æ®¸®°Å »ý¼º
+	// ì´ë²¤íŠ¸ íŠ¸ë¦¬ê±° ìƒì„±
 	UEventTargetInit PalletTownDoorSetting;
 	PalletTownDoorSetting.SetName("PalletTownDoor");
 	PalletTownDoorSetting.SetPoint({ 6, 13 });
@@ -33,6 +34,44 @@ void UInteriorOaksLabLevel::BeginPlay()
 	PalletTownDoor->SetExteriorDoorName("OaksLabDoor");
 	PalletTownDoor->SetTargetMapNameText(L"PALLET TOWN");
 	PalletTownDoor->RegisterPredefinedEvent();
+
+	std::vector<std::wstring> DialogueBigMachine =
+	{
+		LR"(What could this machine be?
+			Better not mess around with it!)"
+	};
+	ADialogueActor* Machine0 = ADialogueActor::GenerateObject(this, "Machine0", { 1, 4 }, EFontColor::Gray, DialogueBigMachine);
+	ADialogueActor* Machine1 = ADialogueActor::GenerateObject(this, "Machine1", { 1, 5 }, EFontColor::Gray, DialogueBigMachine);
+	ADialogueActor* Machine2 = ADialogueActor::GenerateObject(this, "Machine2", { 2, 4 }, EFontColor::Gray, DialogueBigMachine);
+	ADialogueActor* Machine3 = ADialogueActor::GenerateObject(this, "Machine3", { 2, 5 }, EFontColor::Gray, DialogueBigMachine);
+
+	std::vector<std::wstring> DialogueSmallMachine =
+	{
+		LR"(Lights in different colors are
+			flashing on and off.)"
+	};
+	ADialogueActor* Machine4 = ADialogueActor::GenerateObject(this, "Machine4", { 0, 1 }, EFontColor::Gray, DialogueSmallMachine);
+	ADialogueActor* Machine5 = ADialogueActor::GenerateObject(this, "Machine5", { 1, 1 }, EFontColor::Gray, DialogueSmallMachine);
+
+	std::vector<std::wstring> DialogueBookShelf =
+	{
+		LR"(It's crammed full of POKÃ©MON
+			books)"
+	};
+	ADialogueActor* BookShelf0 = ADialogueActor::GenerateObject(this, "BookShelf0", { 9, 1 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf1 = ADialogueActor::GenerateObject(this, "BookShelf1", { 10, 1 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf2 = ADialogueActor::GenerateObject(this, "BookShelf2", { 11, 1 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf3 = ADialogueActor::GenerateObject(this, "BookShelf3", { 12, 1 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf4 = ADialogueActor::GenerateObject(this, "BookShelf4", { 12, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf5 = ADialogueActor::GenerateObject(this, "BookShelf5", { 11, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf6 = ADialogueActor::GenerateObject(this, "BookShelf6", { 10, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf7 = ADialogueActor::GenerateObject(this, "BookShelf7", { 9, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf8 = ADialogueActor::GenerateObject(this, "BookShelf8", { 8, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf9 = ADialogueActor::GenerateObject(this, "BookShelf9", { 4, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf10 = ADialogueActor::GenerateObject(this, "BookShelf10", { 3, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf11 = ADialogueActor::GenerateObject(this, "BookShelf11", { 2, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf12 = ADialogueActor::GenerateObject(this, "BookShelf12", { 1, 8 }, EFontColor::Gray, DialogueBookShelf);
+	ADialogueActor* BookShelf13 = ADialogueActor::GenerateObject(this, "BookShelf13", { 0, 8 }, EFontColor::Gray, DialogueBookShelf);
 }
 
 
