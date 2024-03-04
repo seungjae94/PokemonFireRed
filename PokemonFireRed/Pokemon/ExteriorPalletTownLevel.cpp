6bb2bd775fb5f5718dc42160acf3f2d7f1bca0a3
seungjae94 이승재 <mathlife12345@gmail.com>
2024-03-04 16:34:58 +0900

    BE 스탯 변경 확인

--- PokemonFireRed/Pokemon/ExteriorPalletTownLevel.cpp
+++ PokemonFireRed/Pokemon/ExteriorPalletTownLevel.cpp
@@ -101,9 +101,9 @@ and POKéMON as data via PC.)"
 	});
 
 	// 디버그용 포켓몬 추가 이벤트
-	UPokemon Pokemon0 = UPokemon(EPokedexNo::Charmander, 7);
-	UPokemon Pokemon1 = UPokemon(EPokedexNo::Bulbasaur, 30);
-	UPokemon Pokemon2 = UPokemon(EPokedexNo::Squirtle, 7);
+	UPokemon Pokemon0 = UPokemon(EPokedexNo::Squirtle, 7);
+	UPokemon Pokemon1 = UPokemon(EPokedexNo::Charmander, 7);
+	UPokemon Pokemon2 = UPokemon(EPokedexNo::Bulbasaur, 30);
 	UPokemon Pokemon3 = UPokemon(EPokedexNo::Pidgey, 2);
 	UPokemon Pokemon4 = UPokemon(EPokedexNo::Rattata, 2);
 	UPokemon Pokemon5 = UPokemon(EPokedexNo::Caterpie, 3);
