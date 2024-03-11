b072d57551a3a5ad4543485787cb24ca059d96d2
seungjae94 이승재 <mathlife12345@gmail.com>
2024-03-11 17:00:50 +0900

    회색 체육관에서 회색 시티로 나오는 이벤트 구현

--- PokemonFireRed/Pokemon/InteriorPewterGymLevel.cpp
+++ PokemonFireRed/Pokemon/InteriorPewterGymLevel.cpp
@@ -1,4 +1,5 @@
 #include "InteriorPewterGymLevel.h"
+#include "InteriorDoor.h"
 
 UInteriorPewterGymLevel::UInteriorPewterGymLevel() 
 {
@@ -11,5 +12,22 @@ UInteriorPewterGymLevel::~UInteriorPewterGymLevel()
 void UInteriorPewterGymLevel::BeginPlay()
 {
 	UMapLevel::BeginPlay();
+
+	MakePewterCityDoor();
+}
+
+void UInteriorPewterGymLevel::MakePewterCityDoor()
+{
+	UEventTargetSetting Setting;
+	Setting.SetName("Door");
+	Setting.SetPoint({ 6, 15 });
+
+	AInteriorDoor* Door = SpawnEventTrigger<AInteriorDoor>(Setting);
+	Door->SetTargetMapName(Global::ExteriorPewterCityLevel);
+	Door->SetTargetPoint({ 19, 21 });
+	Door->SetMoveDirection(FTileVector::Down);
+	Door->SetExteriorDoorName(EN::PewterGymDoor);
+	Door->SetTargetMapNameText(L"PEWTER CITY");
+	Door->RegisterPredefinedEvent();
 }
 
