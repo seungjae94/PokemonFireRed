c0f6b96803603db201aa27829499ebfad0d17c09
seungjae94 이승재 <mathlife12345@gmail.com>
2024-03-15 13:37:08 +0900

    울음소리 및 SE 파일 wav 형식에서 mp3로 변환

--- PokemonFireRed/Pokemon/Global.h
+++ PokemonFireRed/Pokemon/Global.h
@@ -154,12 +154,12 @@ namespace RN
 	const std::string BgmOaksTheme = "OaksTheme.mp3";
 
 	// SE
-	const std::string TitleCharizardCry = "CryCharizard.wav";
-	const std::string SEClick = "SEClick.wav";
-	const std::string SEMenu = "SEMenu.wav";
-	const std::string SEExitMap = "SEExitMap.wav";
-	const std::string SEDoorOpen = "SEDoorOpen.wav";
-	const std::string SESurprise = "SESurprise.wav";
+	const std::string TitleCharizardCry = "CryCharizard.mp3";
+	const std::string SEClick = "SEClick.mp3";
+	const std::string SEMenu = "SEMenu.mp3";
+	const std::string SEExitMap = "SEExitMap.mp3";
+	const std::string SEDoorOpen = "SEDoorOpen.mp3";
+	const std::string SESurprise = "SESurprise.mp3";
 	const std::string SEFoundItem = "SEFoundItem.mp3";
 
 	// ¿ÃπÃ¡ˆ
